fe70e619bfb08587f436e2a823174824fdf031a2
readm2 <readm2@readm2ui-MacBook-Pro.local>
2018-05-13 16:25:21 +0900

    Add 5 markers

--- src/main.js
+++ src/main.js
@@ -1,16 +1,44 @@
 window.onload = function() {
     console.log('::::onload');
-    var lat = 37.481;
-    var lng = 126.893;
-    var zoom = 20;
-    var mymap = L.map('map', {
-        center: [lat, lng],
-        zoom: zoom
+
+    var myPlace = {
+        center: [37.4935837,127.0233413],
+        zoom: 50
+    }
+
+    var mapCanvas = L.map('map', {
+        center: myPlace.center,
+        zoom: myPlace.zoom
     });
-    L.tileLayer('http://tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(mymap);
+    L.tileLayer('http://tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(mapCanvas);
+    var locations = [
+        {
+            center: [37.4936131,127.0222611]
+        },
+        {
+            center: [37.4926623,127.0223564]
+        },
+        {
+            center: [37.4935687,127.0211194]
+        },
+        {
+            center: [37.4935837,127.0223413]
+        },
+        {
+            center: [37.4933325,127.0220197]
+        }
+    ];
+    L.marker(locations[0].center).addTo(mapCanvas);
+    L.marker(locations[1].center).addTo(mapCanvas);
+    L.marker(locations[2].center).addTo(mapCanvas);
+    L.marker(locations[3].center).addTo(mapCanvas);
+    L.marker(locations[4].center).addTo(mapCanvas);
+    // displayInitMarker();
 
     ko.applyBindings(new StationViewModel());
 }
+
+var mapCanvas = null;
 
 function Station(data) {
     console.log('ko::::Station ' + data.name);
@@ -29,4 +57,29 @@ function StationViewModel() {
     };
 
     self.removeStation = function(station) { self.stations.remove(station) };
+}
+
+function displayInitMarker() {
+    var locations = [
+        {
+            center: [51.5, -0.09]
+        },
+        {
+            center: [51.495, -0.083]
+        },
+        {
+            center: [51.49, -0.1]
+        },
+        {
+            center: [51.5, -0.09]
+        },
+        {
+            center: [51.5, -0.09]
+        }
+    ];
+    L.marker(locations[0].center).addTo(mapCanvas);
+    L.marker(locations[1].center).addTo(mapCanvas);
+    L.marker(locations[2].center).addTo(mapCanvas);
+    L.marker(locations[3].center).addTo(mapCanvas);
+    L.marker(locations[4].center).addTo(mapCanvas);
 }
